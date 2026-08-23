a9e2b8d1d8fd65e45cd06312b59050ba411a8163
perdanamain <firman.fp123@gmail.com>
2026-08-23 13:28:33 +0700

    feat: initialize career, portfolio, service, and working process APIs with new content types and controllers

---
+++ src/api/career-setting/controllers/career-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * career-setting controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::career-setting.career-setting');
---
+++ src/api/working-process-detail-setting/controllers/working-process-detail-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * working-process-detail-setting controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::working-process-detail-setting.working-process-detail-setting');
